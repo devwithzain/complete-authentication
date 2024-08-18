2bea105380a9f6f116a2d1ffa4b1bceca815e05f
devwithzain <zainsoftwear11@gmail.com>
2024-08-18 16:22:36 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Authentication ToolTip",
+	description: "Authentication ToolTip by devwithzain",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
+}
