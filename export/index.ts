2782d552f4a1128b90069438139bea138dbafaad
devwithzain <ZainSoftwear11@gmail.com>
2024-09-14 05:40:36 +0500

    deleted

--- export/index.ts
+++
@@ -1,9 +0,0 @@
-export { prisma } from '@/lib/prisma';
-export { getUserById } from "@/data/user";
-export { getUserByEmail } from "@/data/user";
-export { login, registerData } from "@/action/user";
-export { default as Socials } from '@/components/socials';
-export { default as LoginForm } from '@/components/login-form';
-export { loginFormSchema, registerFormSchema, } from "@/schemas";
-export { default as RegisterForm } from '@/components/register-form';
-export { default as ToastProvider } from "@/providers/toast-provider";
